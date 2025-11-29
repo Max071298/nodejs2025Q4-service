503d0a42cb8dda5d7528cd817a74930ba344a9cf
MaksRybolt <102859892+Max071298@users.noreply.github.com>
2025-11-29 20:42:06 +0300

    feat: implement /user route

---
+++ src/users/users.controller.ts
@@ -0,0 +1,85 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from './infterfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Controller('user')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll(): Partial<User>[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ): Partial<User> {
+    try {
+      return this.usersService.findOne(id);
+    } catch (e) {
+      if (e.message === 'User not found') {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+    }
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    try {
+      return this.usersService.create(createUserDto);
+    } catch (e) {
+      if (e.message === 'Request body does not contain required fields') {
+        throw new HttpException(
+          'Request body does not contain required fields',
+          HttpStatus.BAD_REQUEST,
+        );
+      } else if (e.message === 'Current user already exists') {
+        throw new HttpException(
+          'Current user already exists',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    }
+  }
+
+  @Put(':id')
+  updatePassword(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    try {
+      return this.usersService.updatePassword(id, updatePasswordDto);
+    } catch (e) {
+      if (e.message === 'User not found') {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      } else if (e.message === 'Invalid old password') {
+        throw new HttpException('Invalid old password', HttpStatus.FORBIDDEN);
+      }
+    }
+  }
+
+  @Delete(':id')
+  delele(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): string {
+    try {
+      return this.usersService.delete(id);
+    } catch (e) {
+      if (e.message === 'User not found') {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+    }
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './infterfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { randomUUID } from 'crypto';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+
+  create(user: CreateUserDto) {
+    const { login, password } = user;
+    if (user === undefined || password === undefined)
+      throw new Error('Request body does not contain required fields');
+    const isALreadyExist = this.users.find((user) => user.id === id);
+    if (isALreadyExist) throw new Error('Current user already exists');
+
+    const id = randomUUID();
+    const version = 0;
+    const createdAt = Date.now();
+    const updatedAt = createdAt;
+    const newUser = { login, password, id, version, createdAt, updatedAt };
+    this.users.push(newUser);
+    return { login, id, version, createdAt, updatedAt };
+  }
+
+  findAll(): Partial<User>[] {
+    return this.users.map((user) => {
+      const { password, ...formattedUser } = user;
+      return formattedUser;
+    });
+  }
+
+  findOne(id: string): Partial<User> {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new Error('User not found');
+    } else {
+      const { password, ...formattedUser } = user;
+      return formattedUser;
+    }
+  }
+
+  updatePassword(id: string, updatePassword: UpdatePasswordDto): Partial<User> {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) throw new Error('User not found');
+    if (user.password !== updatePassword.oldPassword)
+      throw new Error('Invalid old password');
+    user.password = updatePassword.newPassword;
+    user.updatedAt = Date.now();
+    user.version++;
+
+    const { password, ...userToReturn } = user;
+    return userToReturn;
+  }
+
+  delete(id: string): string {
+    const userPos = this.users.findIndex((user) => user.id === id);
+    if (userPos === -1) throw new Error('User not found');
+
+    this.users.splice(userPos, 1);
+
+    return `User with id ${id} successfully deleted`;
+  }
+}
